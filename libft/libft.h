cf94d0a8c889f82efd7fe1d7c67ffd4a02eb1c3a
Charly BOUDRINGHIN <cboudrin@e1r5p16.42.fr>
2021-11-26 10:55:05 +0100

    atoi

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cboudrin <cboudrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 10:06:35 by cboudrin          #+#    #+#             */
-/*   Updated: 2021/11/25 16:46:03 by cboudrin         ###   ########.fr       */
+/*   Updated: 2021/11/26 10:39:10 by cboudrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int				ft_tolower(int c);
 
 char			*ft_strchr(const char *s, int c);
 
-char	*ft_strrchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
 
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 
@@ -53,6 +53,8 @@ void			*ft_memchr(const void *s, int c, size_t n);
 
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char			*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+int				ft_atoi(const char *str);
 
 #endif
