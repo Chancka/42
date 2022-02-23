ef3077226ee9d069efbea07503efb389f80edb2f
Charly BOUDRINGHIN <cboudrin@e2r4p23.clusters.42paris.fr>
2022-02-23 16:19:41 +0100

    correction memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,23 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cboudrin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cboudrin <cboudrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:36:19 by cboudrin          #+#    #+#             */
-/*   Updated: 2021/11/25 10:41:14 by cboudrin         ###   ########.fr       */
+/*   Updated: 2022/02/22 13:05:39 by cboudrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	int		i;
 	char	*dtemp;
 	char	*stemp;
 
 	dtemp = (char *)dest;
 	stemp = (char *)src;
-	i = 0;
 	if (stemp < dtemp)
 	{
 		while (len--)
